3b57eca837a40acbd6d553c38994fa460573bd5e
Samuel Newman <mozzius@protonmail.com>
2024-10-28 22:35:49 +0000

    seems jwt isn't sent (makes sense)

--- main.ts
+++ main.ts
@@ -1,26 +1,14 @@
 import {
-  AtpAgent,
   AtUri,
   type ComAtprotoModerationCreateReport,
   ComAtprotoRepoStrongRef,
 } from "npm:@atproto/api";
-import { decode } from "jsr:@wok/djwt";
+
 import { load } from "jsr:@std/dotenv";
 
 const { SLACK_WEBHOOK } = await load();
 
-const agent = new AtpAgent({
-  service: "https://public.api.bsky.app",
-});
-
 Deno.serve({ port: 6969 }, async (request) => {
-  const auth = request.headers.get("authorization");
-  if (!auth) return new Response("Unauthorized", { status: 401 });
-  const [_header, payload] = decode(auth);
-  if (typeof payload !== "object" || payload === null || !("sub" in payload)) {
-    return new Response("Unauthorized", { status: 401 });
-  }
-  const did = payload.sub as string;
   const pathname = new URL(request.url).pathname;
   switch (pathname) {
     case "/":
@@ -29,7 +17,6 @@ Deno.serve({ port: 6969 }, async (request) => {
       const report = await request.json();
       handleReport(
         report as ComAtprotoModerationCreateReport.InputSchema,
-        did,
       );
       return new Response("ok");
     }
@@ -44,21 +31,17 @@ Deno.serve({ port: 6969 }, async (request) => {
 
 async function handleReport(
   report: ComAtprotoModerationCreateReport.InputSchema,
-  did: string,
 ) {
   if (
     report.subject.type !== "post" ||
     !ComAtprotoRepoStrongRef.isMain(report.subject)
   ) return;
 
-  const user = await agent.app.bsky.actor.getProfile({ actor: did });
-
   const aturi = new AtUri(report.subject.uri);
 
   const message = `
 Reported post: https://bsky.app/profile/${aturi.host}/post/${aturi.rkey}
 
-Reported by: @${user.data.handle}
 Reason: ${report.reasonType.slice("com.atproto.moderation.defs#reason".length)}
 Message:
 ${report.reason}`;
